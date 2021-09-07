474b9ebb79118ce23fe0bd9d535dd74026165a69
Reem Alsenan <reemalsenan@gmail.com>
2021-09-07 12:19:20 +0300

    create project and install dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT;
+const cors = require('cors')
+
+mongoose.connect(process.env.mongoDBURL , {useNewUrlParser:true , useUnifiedTopology: true} ,() => console.log('connected to the database'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+app.use(cors());
+
+
+app.listen(PORT, console.log(`server is running on port ${PORT}`))
